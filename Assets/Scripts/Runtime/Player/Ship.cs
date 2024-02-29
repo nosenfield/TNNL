55c7b0ed39e84438549c59f62188bb586785d293
Noah Rosenfield <nrosenfi@gmail.com>
2024-02-29 16:21:12 -0500

    renames Ship to ShipView and adds documentation on usage

--- Assets/Scripts/Runtime/Player/Ship.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace TNNL.Player
-{
-    public class Ship : MonoBehaviour
-    {
-
-    }
-}
---
+++ Assets/Scripts/Runtime/Player/ShipView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace TNNL.Player
+{
+
+    /// <summary>
+    /// Placed on the Ship game object to separate it from the Shield game object to differentiate collision types
+    /// </summary>
+    public class ShipView : MonoBehaviour
+    {
+
+    }
+}
