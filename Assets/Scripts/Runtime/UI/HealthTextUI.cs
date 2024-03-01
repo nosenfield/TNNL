34f9dc7046fa3f021b3262618f9dc5da483748df
Noah Rosenfield <nrosenfi@gmail.com>
2024-03-01 15:20:22 -0500

    adds HealthTextUI component and related assets.

--- Assets/Scripts/Runtime/UI/HealthTextUI.cs
+++ Assets/Scripts/Runtime/UI/HealthTextUI.cs
@@ -3,6 +3,7 @@ using TMPro;
 using TNNL.Collidables;
 using TNNL.Player;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TNNL.UI
 {
@@ -12,6 +13,8 @@ namespace TNNL.UI
         [SerializeField] Color WarningColor;
         [SerializeField] Color CriticalColor;
         [SerializeField] TextMeshProUGUI healthText;
+        [SerializeField] GameObject criticalAlert;
+        [SerializeField] Image uiHousing;
 
         void Start()
         {
@@ -23,18 +26,33 @@ namespace TNNL.UI
             if (shieldHealth < .3f)
             {
                 healthText.color = CriticalColor;
+                uiHousing.color = CriticalColor;
+                criticalAlert.SetActive(true);
+
             }
             else if (shieldHealth < .5f)
             {
                 healthText.color = WarningColor;
+                uiHousing.color = WarningColor;
+                criticalAlert.SetActive(false);
             }
             else
             {
                 healthText.color = DefaultColor;
+                uiHousing.color = DefaultColor;
+                criticalAlert.SetActive(false);
+            }
+
+            if (shieldHealth >= .1f)
+            {
+                healthText.text = $"{Math.Max(0, Math.Round(shieldHealth * 100)).ToString()}%";
+            }
+            else
+            {
+                healthText.text = $"{Math.Max(0, (shieldHealth * 100)).ToString("0.0")}";
             }
 
 
-            healthText.text = $"{Math.Round(shieldHealth * 100).ToString()}%";
         }
 
     }
