e3bc20132643cdb234d371ebca413e23362328d9
Noah Rosenfield <nrosenfi@gmail.com>
2024-03-14 21:18:59 -0400

    adds border gradient images, BorderGradient script, and animator/animation assets

---
+++ Assets/Scripts/Runtime/UI/BorderGradient.cs
@@ -0,0 +1,27 @@
+using TNNL.Player;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace TNNL.UI
+{
+    public class BorderGradient : MonoBehaviour
+    {
+        [SerializeField] Animator animator;
+
+        void Start()
+        {
+            ShieldModel.HealthUpdate += HealthUpdateListener;
+            ShieldModel.InvincibilityUpdate += InvincibilityUpdateListener;
+        }
+
+        void HealthUpdateListener(float shieldHealth)
+        {
+            animator.SetFloat("shieldHealth", shieldHealth);
+        }
+
+        void InvincibilityUpdateListener(float secondsRemaining)
+        {
+            animator.SetBool("isInvincible", secondsRemaining > 0f);
+        }
+    }
+}
