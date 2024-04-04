baea45a80e94ab98d033b643f880eb9988be131e
Noah Rosenfield <nrosenfi@gmail.com>
2024-04-03 21:18:18 -0400

    updates button sizing and visibility for OverlayUI

--- Assets/Scripts/Runtime/UI/UIToolkit/OverlayUI.cs
+++ Assets/Scripts/Runtime/UI/UIToolkit/OverlayUI.cs
@@ -1,4 +1,5 @@
 using System;
+using TNNL.Level;
 using TNNL.Player;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -7,52 +8,102 @@ namespace TNNL.UI.UIToolkit
 {
     public class OverlayUI : MonoBehaviour
     {
-        Button resetShip;
-        Button resetLevel;
+        private static OverlayUI instance;
+        Button startRun;
+        // Button resetLevel;
         Button nextLevel;
+        Button resetData;
 
-        public static Action StartRunClicked;
-        public static Action ResetLevelClicked;
-        public static Action NextLevelClicked;
+        public static Action StartRunClick;
+        // public static Action ResetLevelClicked;
+        public static Action NextLevelClick;
+        public static Action ResetDataClick;
 
         void OnEnable()
         {
-            VisualElement root = GetComponentInParent<UIDocument>().rootVisualElement;
-            resetShip = root.Q<Button>("StartRunButton");
-            resetLevel = root.Q<Button>("ResetLevelButton");
-            nextLevel = root.Q<Button>("NextLevelButton");
+            instance = this;
+            VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
-            resetShip.clicked += ResetShipClickedHandler;
-            resetLevel.clicked += ResetLevelClickedHandler;
+            startRun = root.Q<Button>("StartRunButton");
+            startRun.clicked += StartRunClickedHandler;
+
+            // // resetLevel = root.Q<Button>("ResetLevelButton");
+            // // resetLevel.clicked += ResetLevelClickedHandler;
+
+            nextLevel = root.Q<Button>("NextLevelButton");
             nextLevel.clicked += NextLevelClickedHandler;
+
+            resetData = root.Q<Button>("ResetDataButton");
+            resetData.clicked += ResetDataClickedHandler;
+
         }
 
         void OnDisable()
         {
             Debug.Log("Remove OverlayUI event listeners");
-
-            resetShip.clicked -= ResetShipClickedHandler;
-            resetLevel.clicked -= ResetLevelClickedHandler;
+            startRun.clicked -= StartRunClickedHandler;
+            // // resetLevel.clicked -= ResetLevelClickedHandler;
             nextLevel.clicked -= NextLevelClickedHandler;
+            resetData.clicked -= ResetDataClickedHandler;
         }
 
-        void ResetShipClickedHandler()
+        void StartRunClickedHandler()
         {
-            Debug.Log("ResetShipClickedHandler");
-            StartRunClicked?.Invoke();
+            Debug.Log("StartRunClickedHandler");
+            StartRunClick?.Invoke();
         }
 
-        void ResetLevelClickedHandler()
-        {
-            Debug.Log("ResetLevelClickedHandler");
-            ResetLevelClicked?.Invoke();
-        }
+        // void ResetLevelClickedHandler()
+        // {
+        //     Debug.Log("ResetLevelClickedHandler");
+        //     ResetLevelClicked?.Invoke();
+        // }
 
         void NextLevelClickedHandler()
         {
             Debug.Log("NextLevelClickedHandler");
-            NextLevelClicked?.Invoke();
+            NextLevelClick?.Invoke();
         }
 
+        void ResetDataClickedHandler()
+        {
+            Debug.Log("ResetDataClickedHandler");
+            ResetDataClick?.Invoke();
+        }
+
+        public static void SetVisible(bool visible)
+        {
+            instance.GetComponentInParent<UIDocument>().rootVisualElement.style.display = visible ? DisplayStyle.Flex : DisplayStyle.None;
+            // instance.gameObject.SetActive(visible);
+        }
+
+        public static void SetRunButtonCopy(string copy)
+        {
+            instance.startRun.text = $"\n{copy}\n";
+        }
+
+        public static void SetDebugState()
+        {
+            Debug.Log($"SetDebugState()");
+            instance.startRun.visible = true;
+            // instance.nextLevel.visible = true;
+            instance.resetData.visible = true;
+        }
+
+        public static void SetGameplayActiveState()
+        {
+            Debug.Log($"SetGameplayActiveState()");
+            instance.startRun.visible = true;
+            // instance.nextLevel.visible = false;
+            instance.resetData.visible = false;
+        }
+
+        public static void SetGameplayResetState()
+        {
+            Debug.Log($"SetGameplayResetState()");
+            instance.startRun.visible = true;
+            // instance.nextLevel.visible = true;
+            instance.resetData.visible = true;
+        }
     }
 }
