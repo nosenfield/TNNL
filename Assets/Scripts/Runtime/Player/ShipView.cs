193b49354f28a7222d8ec72ff0f2228ab8fcf6e7
Noah Rosenfield <nrosenfi@gmail.com>
2024-04-03 22:05:38 -0400

    deletes unused ShipController class. removes unused code from ShipView

--- Assets/Scripts/Runtime/Player/ShipView.cs
+++ Assets/Scripts/Runtime/Player/ShipView.cs
@@ -1,6 +1,3 @@
-using System;
-using Sirenix.OdinInspector;
-using TNNL.Collidables;
 using UnityEngine;
 
 namespace TNNL.Player
@@ -9,32 +6,5 @@ namespace TNNL.Player
     /// <summary>
     /// Placed on the Ship game object to separate it from the Shield game object to differentiate collision types
     /// </summary>
-    public class ShipView : MonoBehaviour
-    {
-        public Action<AbstractCollidable> ShipCollision;
-
-        // mvc 
-        [SerializeField] private bool GenerateController;
-        [SerializeField][ReadOnly] ShipController controller;
-
-        void Awake()
-        {
-            if (GenerateController)
-            {
-                controller = new ShipController(this);
-            }
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            AbstractCollidable collidable = other.GetComponentInParent<AbstractCollidable>();
-
-            if (collidable != null && !collidable.Dirty)
-            {
-                ShipCollision?.Invoke(collidable);
-            }
-
-            // parse/report the collision by some kind of type identifier (what did we hit)
-        }
-    }
+    public class ShipView : MonoBehaviour { }
 }
